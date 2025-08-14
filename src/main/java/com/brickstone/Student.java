4f87d9dc45a417fb5c31861470d8481f97a20e83
Atul Ware <eratulware@gmail.com>
2025-08-14 12:11:25 +0530

    Data fetching from db

--- src/main/java/com/brickstone/Student.java
+++ src/main/java/com/brickstone/Student.java
@@ -1,7 +1,11 @@
 package com.brickstone;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
-	
+	@Id
 	private int sid;
 	private String sname;
 	private int spoint;
